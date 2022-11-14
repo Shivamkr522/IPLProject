b8c6b6e00653580c57649edf0cce2d1b12ccdbcf
Shivamkr522 <105207334+Shivamkr522@users.noreply.github.com>
2022-11-14 11:34:18 +0530

    Final Second

--- secondTask.java
+++ secondTask.java
@@ -1,19 +1,8 @@
-
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.*;
 
 public class secondTask {
-    public static void main(String[] args){
-        try{
-            BufferedReader read = new BufferedReader(new FileReader("/home/shivam/Project1/matches.csv"));
-            List<String> lines = new ArrayList<>();
-            String line;
-            while((line = read.readLine())!=null){
-                lines.add(line);
-            }
-            lines.remove(0);
-            //System.out.println(lines);
+    static void matchesWonByTeamOverYears(List<String> lines){
+
             HashMap<String,Integer> teamWonOverYears = new HashMap<>();
             for( String s : lines){
                 List<String> str = new ArrayList<>(Arrays.asList(s.split(",")));
@@ -25,14 +14,9 @@ public class secondTask {
                     teamWonOverYears.put(str.get(1)+" "+str.get(10),1);
                 }
             }
-            //System.out.println(teamWonOverYears);
             for(Map.Entry m : teamWonOverYears.entrySet()){
                 System.out.println(m.getKey()+" "+m.getValue());
             }
-        }
-        catch(Exception e){
-            e.getStackTrace();
-        }
 
     }
 }
